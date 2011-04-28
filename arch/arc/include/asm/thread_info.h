f267c8f588d9c662aaf572ff13ae2da04dc36c95
vineetg <vineetg@4eed8e25-bc12-0410-818a-f70ee6866280>
2011-04-28 13:11:30 +0000

    ARC: miscll fixes: generic statfs, cacheflush syscall, 1 word sigset_t
    -Backport from Android kernel
    	=generic statfs (also requires uClibc hdr change)
    	=cacheflush syscall for JITs
    	=allowing a shorter sigset_t (1 word as opposed to normal 2 words)
    	=FREEZER define
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/include/asm/thread_info.h
+++ arch/arc/include/asm/thread_info.h
@@ -128,6 +128,7 @@ static inline __attribute_const__ struct thread_info *current_thread_info(void)
 #define TIF_USEDFPU     16  /* FPU was used by this task this quantum (SMP) */
 #define TIF_POLLING_NRFLAG  17  /* true if poll_idle() is polling TIF_NEED_RESCHED */
 #define TIF_MEMDIE      18
+#define TIF_FREEZE      19
 #define TIF_SYSCALL_TRACE   31  /* syscall trace active */
 
 #define _TIF_SYSCALL_TRACE  (1<<TIF_SYSCALL_TRACE)
@@ -139,6 +140,7 @@ static inline __attribute_const__ struct thread_info *current_thread_info(void)
 #define _TIF_RESTORE_SIGMASK    (1<<TIF_RESTORE_SIGMASK)
 #define _TIF_USEDFPU        (1<<TIF_USEDFPU)
 #define _TIF_POLLING_NRFLAG (1<<TIF_POLLING_NRFLAG)
+#define _TIF_FREEZE         (1<<TIF_FREEZE)
 
 /* work to do on interrupt/exception return */
 #define _TIF_WORK_MASK      (0x0000ffef & ~_TIF_SECCOMP)
