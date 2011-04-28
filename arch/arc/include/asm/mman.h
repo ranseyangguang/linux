f41320205be2bb9fec9866022a039d82bf2fe354
vineetg <vineetg@4eed8e25-bc12-0410-818a-f70ee6866280>
2011-04-28 13:23:27 +0000

    ARC: cleanups: In preparation of shared lib common vaddr mappings
    -moved some Kconfig items out of processor section
    -arch specific mmap wrapper moved to mmap.c
    -do_mmap( ) signature changed - long to unsigned long
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/include/asm/mman.h
+++ arch/arc/include/asm/mman.h
@@ -1,14 +1,13 @@
-/* Sameer: Previously this file was 2.4-i386 clone which is
-           now 2.6-i386 clone */
 #ifndef __ARC_MMAN_H__
 #define __ARC_MMAN_H__
 
+#include <linux/fs.h>
 #include <asm-generic/mman.h>
 
 #define MAP_GROWSDOWN	0x0100		/* stack-like segment */
 #define MAP_DENYWRITE	0x0800		/* ETXTBSY */
 #define MAP_EXECUTABLE	0x1000		/* mark it as an executable */
-#define MAP_LOCKED	0x2000		/* pages are locked */
+#define MAP_LOCKED	    0x2000		/* pages are locked */
 #define MAP_NORESERVE	0x4000		/* don't check for reservations */
 #define MAP_POPULATE	0x8000		/* populate (prefault) pagetables */
 #define MAP_NONBLOCK	0x10000		/* do not block on IO */
