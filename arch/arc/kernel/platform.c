b9ae563d11046647294fc9c28bdfcf8d684965a5
vineetg <vineetg@4eed8e25-bc12-0410-818a-f70ee6866280>
2011-04-28 12:36:36 +0000

    ARC: Virtual Ethernet Driver for Instruction Set Simulator
    -contributed by Simon Spooner
    -Simple support right now, single packet in, single packet out.
    -On my ISS I get sub 5MS pings, so good enough.
    -NFS works fine, no timeouts.
    -You'll need the enet_inst.dll and some config.
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/kernel/platform.c
+++ arch/arc/kernel/platform.c
@@ -2,12 +2,12 @@
 #include <linux/err.h>
 #include <linux/init.h>
 
+#ifdef CONFIG_XILINX_TEMAC
+
 static __init add_xtemac(void)
 {
     struct platform_device *pd;
-printk("ADDING XTEMAC\n");
     pd = platform_device_register_simple("xilinx_temac",0,NULL,0);
-printk("PD is %x\n", pd);
 
     if (IS_ERR(pd))
     {
@@ -18,3 +18,22 @@ printk("PD is %x\n", pd);
 }
 
 device_initcall(add_xtemac);
+
+#endif
+
+#ifdef CONFIG_ISS_MAC
+static __init add_iss(void)
+{
+    struct platform_device *pd;
+    pd = platform_device_register_simple("arc_iss",0,NULL,0);
+
+    if (IS_ERR(pd))
+    {
+        printk("Fail\n");
+    }
+
+    return IS_ERR(pd) ? PTR_ERR(pd): 0;
+}
+
+device_initcall(add_iss);
+#endif
