68d9d0abcabf90f9201739135876fdc04b02009c
vgupta <vgupta@4eed8e25-bc12-0410-818a-f70ee6866280>
2011-07-30 11:17:32 +0000

    ARC: cleanup: duplicate #defs
    Surprisingly there was no build breakage since none of the duplicate defs were
    actually being used in code
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/include/asm/mman.h
+++ arch/arc/include/asm/mman.h
@@ -12,22 +12,13 @@
 #include <linux/fs.h>
 #include <asm-generic/mman.h>
 
-#define MAP_GROWSDOWN	0x0100		/* stack-like segment */
-#define MAP_DENYWRITE	0x0800		/* ETXTBSY */
-#define MAP_EXECUTABLE	0x1000		/* mark it as an executable */
-#define MAP_LOCKED	    0x2000		/* pages are locked */
-#define MAP_NORESERVE	0x4000		/* don't check for reservations */
-#define MAP_POPULATE	0x8000		/* populate (prefault) pagetables */
-#define MAP_NONBLOCK	0x10000		/* do not block on IO */
-
 #define MAP_SHARED_CODE 0x20000
 
-#define MCL_CURRENT	1		/* lock all current mappings */
-#define MCL_FUTURE	2		/* lock all future mappings */
-
+#ifdef __KERNEL__
 #define ARCH_ELF_DO_MMAP
 extern unsigned long do_mmap(struct file *file, unsigned long addr,
 	unsigned long len, unsigned long prot,
 	unsigned long flag, unsigned long offset);
+#endif
 
 #endif /* __ARC_MMAN_H__ */
