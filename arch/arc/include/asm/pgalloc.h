955b227bfd23dfd52c750464000cab8bfc2dc171
Vineet Gupta <vgupta@synopsys.com>
2012-02-13 13:46:28 +0530

    ARC: refactor: consistent usage of pgtable_t in pgalloc.h
    -We were not using pgtable_t at places where we shd
    -Doing so will allow future optimisation to make it void * instead of
     struct page * which is slightly costly
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/include/asm/pgalloc.h
+++ arch/arc/include/asm/pgalloc.h
@@ -33,7 +33,7 @@ pmd_populate_kernel(struct mm_struct *mm, pmd_t *pmd, pte_t *pte)
 }
 
 static inline void
-pmd_populate(struct mm_struct *mm, pmd_t *pmd, struct page *ptep)
+pmd_populate(struct mm_struct *mm, pmd_t *pmd, pgtable_t ptep)
 {
     pmd_set(pmd, (pte_t *)(unsigned long)page_address(ptep));
 }
@@ -99,10 +99,10 @@ pte_alloc_one_kernel(struct mm_struct *mm, unsigned long address)
     return pte;
 }
 
-static inline struct page *
+static inline pgtable_t
 pte_alloc_one(struct mm_struct *mm, unsigned long address)
 {
-    struct page *pte_pg;
+    pgtable_t pte_pg;
 
     pte_pg = alloc_pages(GFP_KERNEL | __GFP_REPEAT, PTE_ORDER);
     if (pte_pg)
@@ -119,9 +119,9 @@ static inline void pte_free_kernel(struct mm_struct *mm, pte_t *pte)
     free_pages((unsigned long)pte, PTE_ORDER);  // takes phy addr
 }
 
-static inline void pte_free(struct mm_struct *mm, struct page *ptep)
+static inline void pte_free(struct mm_struct *mm, pgtable_t ptep)
 {
-    __free_page(ptep);
+    __free_page(ptep);  //  takes struct page*
 }
 
 
