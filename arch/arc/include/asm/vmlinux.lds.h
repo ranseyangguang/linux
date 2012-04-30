ee06ce7b25f7517d1c4a370371ecbad3554b5edb
Vineet Gupta <vgupta@synopsys.com>
2012-04-30 16:36:43 +0530

    ARC: tree-wide refactoring: CCM cleanups
    For "hot" routines/code such a TLB Refill handlers, the code/data
    was laid out in special (.text|.data).arcfp sections
    -If (I|D)CCM were configured these were mapped to their wired addr.
    -If not, these subsections were absorbed into corresponding container
     sections (.text/.data respectively).
    
    Instead now, we don't generate these sub-sections if CCMs ae not
    configured
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/include/asm/vmlinux.lds.h
+++ arch/arc/include/asm/vmlinux.lds.h
@@ -9,20 +9,4 @@
 #ifndef __ASM_VMLINUX_LDS_H__
 #define __ASM_VMLINUX_LDS_H__
 
-#ifdef CONFIG_ARC_USE_DCCM
-#define ARCFP_CCM_DATA      *(.data.arcfp)
-#define ARCFP_SDRAM_DATA
-#else
-#define ARCFP_CCM_DATA
-#define ARCFP_SDRAM_DATA    *(.data.arcfp)
-#endif
-
-#ifdef CONFIG_ARC_USE_ICCM
-#define ARCFP_CCM_TEXT      *(.text.arcfp)
-#define ARCFP_SDRAM_TEXT
-#else
-#define ARCFP_CCM_TEXT
-#define ARCFP_SDRAM_TEXT    *(.text.arcfp)
-#endif
-
 #endif
