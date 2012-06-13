9822635b8ba3fd0a5e4bd20d3a4a71858ecddd01
Vineet Gupta <vgupta@synopsys.com>
2012-06-13 17:53:26 +0530

    ARC: Build breakage with CONFIG_ARC_ADDR_SPACE_RND
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/include/asm/exec.h
+++ arch/arc/include/asm/exec.h
@@ -10,26 +10,6 @@
 #ifndef __ASM_ARC_EXEC_H
 #define __ASM_ARC_EXEC_H
 
-static inline unsigned long arch_align_stack(unsigned long sp)
-{
-#ifdef CONFIG_ARC_ADDR_SPACE_RND
-	/* ELF loader sets this flag way early.
-	 * So no need to check for multiple things like
-	 *   !(current->personality & ADDR_NO_RANDOMIZE)
-	 *   randomize_va_space
-	 */
-	if (current->flags & PF_RANDOMIZE) {
-
-		/* Stack grows down for ARC */
-		sp -= get_random_int() & ~PAGE_MASK;
-	}
-#endif
-
-	/* always align stack to 16 bytes */
-	sp &= ~0xF;
-
-	return sp;
-}
-
+extern unsigned long arch_align_stack(unsigned long sp);
 
 #endif
