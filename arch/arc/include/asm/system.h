dd4ed65513c07b270fa3d6f3290a3e1921d4a7b5
Vineet Gupta <vgupta@synopsys.com>
2012-06-13 12:44:33 +0530

    ARC: Reintroduce system.h hack to enable non-converted 3.2 kernel build
    With prev commit backported from tip (~3.5), all ARC code has been fixed
    to not refer to asm/system.h, however the rest of generic code hasn't.
    So to keep it happy, introduce a system.h stub which simply aggregates
    the forked off headers to provide the same APIs as before but we retain
    the advantage of sorked headers - allow clean merges between 3.2 and tip
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

---
+++ arch/arc/include/asm/system.h
@@ -0,0 +1,12 @@
+/* Do not add anything - this will be deleted post 3.2 */
+
+#ifndef __ASM_ARC_SYSTEM_H
+#define __ASM_ARC_SYSTEM_H
+
+#include <asm/atomic.h>
+#include <asm/barrier.h>
+#include <asm/cmpxchg.h>
+#include <asm/exec.h>
+#include <asm/switch_to.h>
+
+#endif
