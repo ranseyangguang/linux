2008cac3be27af3272182acfd65139917cc12c88
Vineet Gupta <vgupta@synopsys.com>
2012-08-14 18:26:00 +0530

    ARC: struct user_regs_struct cleanups
    In user_regs_struct, the opencoded struct pt_regs/callee_regs now
    replaced with a actual structures to keep such info centralised to one
    single file ptrace.h
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/include/asm/user.h
+++ arch/arc/include/asm/user.h
@@ -38,56 +38,8 @@
 
 /* User mode registers, used for core dumps. */
 struct user_regs_struct {
-	/* ------- from struct pt_regs --------- */
-
-	/*
-	 * 1 word gutter after reg-file has been saved
-	 * Technically not needed, Since SP always points to a "full" location
-	 * (vs. "empty"). But pt_regs is shared with tools....
-	 */
-	long reserved1;
-	long bta;	/* bta_l1, bta_l2, erbta */
-	long lp_start;
-	long lp_end;
-	long lp_count;
-	long status32;	/* status32_l1, status32_l2, erstatus */
-	long ret;	/* ilink1, ilink2 or eret */
-	long blink;
-	long fp;
-	long r26;	/* gp */
-	long r12;
-	long r11;
-	long r10;
-	long r9;
-	long r8;
-	long r7;
-	long r6;
-	long r5;
-	long r4;
-	long r3;
-	long r2;
-	long r1;
-	long r0;
-	long sp;	/* user/ kernel sp, depending on where we came from  */
-	long orig_r0;
-	long orig_r8;	/*to distinguish bet excp, sys call, int1 or int2 */
-
-	/* -------- from struct callee_regs --------- */
-	long reserved2;
-	long r25;
-	long r24;
-	long r23;
-	long r22;
-	long r21;
-	long r20;
-	long r19;
-	long r18;
-	long r17;
-	long r16;
-	long r15;
-	long r14;
-	long r13;
-
+	struct pt_regs scratch;
+	struct callee_regs callee;
 	long efa;	/* break pt addr, for break points in delay slots */
 	long stop_pc;	/* give dbg stop_pc directly after checking orig_r8 */
 };
