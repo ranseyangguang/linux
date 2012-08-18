78f2dc354c151849e757cdcc2d37c1f20c420858
Vineet Gupta <vgupta@synopsys.com>
2012-08-18 17:16:37 +0530

    ARC: [plat-arcfpga] Nuke non-necessary arch/arc/drivers/*
    Retained IDE/EMAC for AA4 board to keep doing testing. However they too
    are moved into plat-arcfpga
    
    Once we move away from AA4, these will not be needed anyhow.
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/plat-arcfpga/include/plat/dma_addr.h
+++ arch/arc/plat-arcfpga/include/plat/dma_addr.h
@@ -5,23 +5,18 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  *
- * Vineetg: Feb 2009
- *  -Reworked the API to work with ARC PCI Bridge
- *
  * vineetg: Feb 2009
  *  -For AA4 board, kernel to DMA address APIs
  */
 
-/* Some notes on DMA <=> kernel address generation
+/*
+ * Some notes on DMA <=> kernel address generation
  *
  * A simplistic implementation will generate 0 based bus address.
  * For e.g. 0x8AAA_0000 becomes 0x0AAA_0000 bus addr
  * However this doesnt work with PCI devices behind the PCI Host Bridge on AA4
  * which can't allow 0 based addresses. So the API for special case of PCI
  * makes corrections
- *
- * As a small optimisation, if PCI is not enabled we can simply return
- * 0 based bus addr hence the CONFIG_xx check for PCI Host Bridge
  */
 
 #ifndef __PLAT_DMA_ADDR_H
@@ -32,11 +27,6 @@
 static inline unsigned long plat_dma_addr_to_kernel(struct device *dev,
 						    dma_addr_t dma_addr)
 {
-#ifdef CONFIG_ARC_AHB_PCI_BRIDGE
-	if (dev && dev->bus && (strncmp(dev->bus->name, "pci", 3) == 0))
-		return (unsigned long)dma_addr;
-#endif
-
 	return dma_addr + PAGE_OFFSET;
 }
 
@@ -51,14 +41,11 @@ static inline dma_addr_t plat_kernel_addr_to_dma(struct device *dev, void *ptr)
 	 * ideally we want to check this range here, but our implementation is
 	 * better as it checks for even worse user virtual address as well.
 	 */
-	BUG_ON(addr < PAGE_OFFSET);
+	if (likely(addr >= PAGE_OFFSET))
+		return addr - PAGE_OFFSET;
 
-#ifdef CONFIG_ARC_AHB_PCI_BRIDGE
-	if (dev && dev->bus && (strncmp(dev->bus->name, "pci", 3) == 0))
-		return addr;
-#endif
-
-	return addr - PAGE_OFFSET;
+	BUG();
+	return addr;
 }
 
 #endif
