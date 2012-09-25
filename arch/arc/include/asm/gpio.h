a7ea76f286a36d989215216aa11a88794f584b92
Frank Dols <frank.dols@synopsys.com>
2012-09-25 14:11:21 +0200

    ARC: Prepare for GPIO support
    Signed-off-by: Frank Dols <frank.dols@synopsys.com>

---
+++ arch/arc/include/asm/gpio.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2012 Synopsys, Inc. (www.synopsys.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+
+#ifndef _ARCH_ARC_GPIO_H
+#define _ARCH_ARC_GPIO_H
+
+/* not all ARC platforms necessarily support this API ... */
+#include <plat/gpio.h>
+
+#endif /* _ARCH_ARC_GPIO_H */
