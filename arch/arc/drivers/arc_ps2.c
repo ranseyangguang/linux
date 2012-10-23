61c26efc13faf1277a8d7ff1fafed3e242f2d4eb
Mischa Jonker <mischa.jonker@synopsys.com>
2012-10-23 10:13:33 +0200

    ARC: PS/2 - Update after comments from open source community
    * dev_dbg, dev_info instead of pr_*
    * module_platform_driver() instead of explicit init()/exit()
    * ioread32/iowrite32 instead of inl/outl
    * updated error return values to correct ones
    * add timeout to check_rx (in case of bad HW)
    * reordering of code/cosmetics by Dmitry Torokhov
    * minor things
    
    Signed-off-by: Mischa Jonker <mischa.jonker@synopsys.com>

--- arch/arc/drivers/arc_ps2.c
+++ arch/arc/drivers/arc_ps2.c
@@ -4,6 +4,8 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
+ *
+ * Driver is originally developed by Pavel Sokolov <psokolov@synopsys.com>
  */
 
 #include <linux/module.h>
@@ -15,11 +17,11 @@
 #include <linux/kernel.h>
 #include <linux/slab.h>
 
-#define ARC_PS2_PORTS                   (2)
+#define ARC_PS2_PORTS                   2
 
-#define ARC_ARC_PS2_ID                  (0x0001f609)
+#define ARC_ARC_PS2_ID                  0x0001f609
 
-#define STAT_TIMEOUT                    (128)
+#define STAT_TIMEOUT                    128
 
 #define PS2_STAT_RX_FRM_ERR             (1)
 #define PS2_STAT_RX_BUF_OVER            (1 << 1)
@@ -29,7 +31,8 @@
 #define PS2_STAT_TX_INT_EN              (1 << 5)
 
 struct arc_ps2_port {
-	unsigned data, status;
+	void __iomem *data_addr;
+	void __iomem *status_addr;
 	struct serio *io;
 };
 
@@ -37,24 +40,27 @@ struct arc_ps2_data {
 	struct arc_ps2_port port[ARC_PS2_PORTS];
 	struct resource *iomem_res;
 	int irq;
-	unsigned long addr;
-	unsigned frame_error;
-	unsigned buf_overflow;
-	unsigned total_int;
+	void __iomem *addr;
+	unsigned int frame_error;
+	unsigned int buf_overflow;
+	unsigned int total_int;
 };
 
 static void arc_ps2_check_rx(struct arc_ps2_data *arc_ps2,
 			     struct arc_ps2_port *port)
 {
-	unsigned flag, status;
+	unsigned int timeout = 1000;
+	unsigned int flag, status;
 	unsigned char data;
 
 	do {
-		status = inl(port->status);
+		status = ioread32(port->status_addr);
 		if (!(status & PS2_STAT_RX_VAL))
-			break;
-		flag = 0;
+			return;
 
+		data = ioread32(port->data_addr) & 0xff;
+
+		flag = 0;
 		arc_ps2->total_int++;
 		if (status & PS2_STAT_RX_FRM_ERR) {
 			arc_ps2->frame_error++;
@@ -63,10 +69,11 @@ static void arc_ps2_check_rx(struct arc_ps2_data *arc_ps2,
 			arc_ps2->buf_overflow++;
 			flag |= SERIO_FRAME;
 		}
-		data = inl(port->data) & 0xff;
 
 		serio_interrupt(port->io, data, flag);
-	} while (1);
+	} while (--timeout);
+
+	dev_err(&port->io->dev, "PS/2 hardware stuck\n");
 }
 
 static irqreturn_t arc_ps2_interrupt(int irq, void *dev)
@@ -87,25 +94,26 @@ static int arc_ps2_write(struct serio *io, unsigned char val)
 	int timeout = STAT_TIMEOUT;
 
 	do {
-		status = inl(port->status);
+		status = ioread32(port->status_addr);
 		cpu_relax();
-		timeout--;
-	} while (!(status & PS2_STAT_TX_ISNOT_FUL) && timeout);
 
-	if (timeout)
-		outl(val & 0xff, port->data);
-	else {
-		pr_err("%s: write timeout\n", __func__);
-		return -1;
-	}
+		if (status & PS2_STAT_TX_ISNOT_FUL) {
+			iowrite32(val & 0xff, port->data_addr);
+			return 0;
+		}
 
-	return 0;
+	} while (--timeout);
+
+	dev_err(&io->dev, "write timeout\n");
+	return -ETIMEDOUT;
 }
 
 static int arc_ps2_open(struct serio *io)
 {
 	struct arc_ps2_port *port = io->port_data;
-	outl(PS2_STAT_RX_INT_EN, port->status);
+
+	iowrite32(PS2_STAT_RX_INT_EN, port->status_addr);
+
 	return 0;
 }
 
@@ -113,145 +121,142 @@ static void arc_ps2_close(struct serio *io)
 {
 	struct arc_ps2_port *port = io->port_data;
 
-	outl(inl(port->status) & ~PS2_STAT_RX_INT_EN, port->status);
+	iowrite32(ioread32(port->status_addr) & ~PS2_STAT_RX_INT_EN,
+		  port->status_addr);
 }
 
-int __devinit arc_ps2_allocate_port(struct arc_ps2_port *port, int index,
-				    unsigned base)
+static int __devinit arc_ps2_create_port(struct platform_device *pdev,
+					 struct arc_ps2_data *arc_ps2,
+					 int index)
 {
-	port->io = kzalloc(sizeof(struct serio), GFP_KERNEL);
-	if (!port->io)
-		return -1;
+	struct arc_ps2_port *port = &arc_ps2->port[index];
+	struct serio *io;
 
-	port->io->id.type = SERIO_8042;
-	port->io->write = arc_ps2_write;
-	port->io->open = arc_ps2_open;
-	port->io->close = arc_ps2_close;
-	snprintf(port->io->name, sizeof(port->io->name), "ARC PS/2 port%d",
-		 index);
-	snprintf(port->io->phys, sizeof(port->io->phys), "arc/serio%d", index);
-	port->io->port_data = port;
+	io = kzalloc(sizeof(struct serio), GFP_KERNEL);
+	if (!io)
+		return -ENOMEM;
 
-	port->data = base + 4 + index * 4;
-	port->status = base + 4 + ARC_PS2_PORTS * 4 + index * 4;
+	io->id.type = SERIO_8042;
+	io->write = arc_ps2_write;
+	io->open = arc_ps2_open;
+	io->close = arc_ps2_close;
+	snprintf(io->name, sizeof(io->name), "ARC PS/2 port%d", index);
+	snprintf(io->phys, sizeof(io->phys), "arc/serio%d", index);
+	io->port_data = port;
 
-	pr_debug("%s: port%d is allocated (data = 0x%x, status = 0x%x)\n",
-		 __func__, index, port->data, port->status);
+	port->io = io;
 
+	port->data_addr = arc_ps2->addr + 4 + index * 4;
+	port->status_addr = arc_ps2->addr + 4 + ARC_PS2_PORTS * 4 + index * 4;
+
+	dev_dbg(&pdev->dev, "port%d is allocated (data = 0x%p, status = 0x%p)\n",
+		index, port->data_addr, port->status_addr);
+
+	serio_register_port(port->io);
 	return 0;
 }
 
 static int __devinit arc_ps2_probe(struct platform_device *pdev)
 {
 	struct arc_ps2_data *arc_ps2;
-	int ret, id, i;
+	int error, id, i;
 
 	arc_ps2 = kzalloc(sizeof(struct arc_ps2_data), GFP_KERNEL);
 	if (!arc_ps2) {
-		pr_err("%s: ERROR: out of memory\n", __func__);
-		ret = -ENOMEM;
-		goto out;
+		dev_err(&pdev->dev, "out of memory\n");
+		return -ENOMEM;
 	}
 
 	arc_ps2->iomem_res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	if (!arc_ps2->iomem_res) {
-		pr_err("%s: ERROR: no IO memory defined\n", __func__);
-		ret = -ENODEV;
-		goto out_free;
+		dev_err(&pdev->dev, "no IO memory defined\n");
+		error = -EINVAL;
+		goto err_free_mem;
 	}
 
 	arc_ps2->irq = platform_get_irq_byname(pdev, "arc_ps2_irq");
 	if (arc_ps2->irq < 0) {
-		pr_err("%s: ERROR: no IRQ defined\n", __func__);
-		ret = -ENODEV;
-		goto out_free;
+		dev_err(&pdev->dev, "no IRQ defined\n");
+		error = -EINVAL;
+		goto err_free_mem;
 	}
 
 	if (!request_mem_region(arc_ps2->iomem_res->start,
 	    resource_size(arc_ps2->iomem_res), pdev->name)) {
-		pr_err("%s: ERROR: memory allocation failed cannot get the I/O addr 0x%x\n",
-		       __func__, (unsigned int)arc_ps2->iomem_res->start);
+		dev_err(&pdev->dev, "memory region allocation failed for %pR\n",
+			arc_ps2->iomem_res);
 
-		ret = -EBUSY;
-		goto out_free;
+		error = -EBUSY;
+		goto err_free_mem;
 	}
 
-	arc_ps2->addr = (unsigned long) ioremap_nocache(arc_ps2->iomem_res->
-			start, resource_size(arc_ps2->iomem_res));
+	arc_ps2->addr = ioremap_nocache(arc_ps2->iomem_res->start,
+					resource_size(arc_ps2->iomem_res));
 	if (!arc_ps2->addr) {
-		pr_err("%s: ERROR: memory mapping failed\n", __func__);
-		ret = -ENOMEM;
-		goto out_release_region;
+		dev_err(&pdev->dev, "memory mapping failed\n");
+		error = -ENOMEM;
+		goto err_release_region;
 	}
 
-	pr_info("%s: irq = %d, address = 0x%lx, ports = %i\n", __func__,
-	       arc_ps2->irq, arc_ps2->addr, ARC_PS2_PORTS);
+	dev_info(&pdev->dev, "irq = %d, address = 0x%p, ports = %i\n",
+		 arc_ps2->irq, arc_ps2->addr, ARC_PS2_PORTS);
 
-	id = inl(arc_ps2->addr);
+	id = ioread32(arc_ps2->addr);
 	if (id != ARC_ARC_PS2_ID) {
-		pr_err("%s: device id does not match\n", __func__);
-		ret = ENXIO;
-		goto out_unmap;
+		dev_err(&pdev->dev, "device id does not match\n");
+		error = -ENXIO;
+		goto err_unmap;
+	}
+
+	for (i = 0; i < ARC_PS2_PORTS; i++) {
+		error = arc_ps2_create_port(pdev, arc_ps2, i);
+		if (error)
+			goto err_unregister_ports;
+	}
+
+	error = request_irq(arc_ps2->irq, arc_ps2_interrupt, 0,
+			    "arc_ps2", arc_ps2);
+	if (error) {
+		dev_err(&pdev->dev, "Could not allocate IRQ\n");
+		goto err_unregister_ports;
 	}
 
 	platform_set_drvdata(pdev, arc_ps2);
 
-	for (i = 0; i < ARC_PS2_PORTS; i++) {
-		if (arc_ps2_allocate_port(&arc_ps2->port[i], i,
-					  arc_ps2->addr)) {
-			ret = -ENOMEM;
-			goto release;
-		}
-		serio_register_port(arc_ps2->port[i].io);
-	}
-
-	ret = request_irq(arc_ps2->irq, arc_ps2_interrupt, 0,
-			  "arc_ps2", arc_ps2);
-	if (ret) {
-		pr_err("%s: Could not allocate IRQ\n", __func__);
-		goto release;
-	}
-
 	return 0;
 
-release:
+err_unregister_ports:
 	for (i = 0; i < ARC_PS2_PORTS; i++) {
 		if (arc_ps2->port[i].io)
 			serio_unregister_port(arc_ps2->port[i].io);
 	}
-out_unmap:
-	iounmap((void __iomem *) arc_ps2->addr);
-out_release_region:
+err_unmap:
+	iounmap(arc_ps2->addr);
+err_release_region:
 	release_mem_region(arc_ps2->iomem_res->start,
 			   resource_size(arc_ps2->iomem_res));
-out_free:
+err_free_mem:
 	kfree(arc_ps2);
-out:
-	return ret;
+	return error;
 }
 
-static int __devexit arc_ps2_remove(struct platform_device *dev)
+static int __devexit arc_ps2_remove(struct platform_device *pdev)
 {
-	struct arc_ps2_data *arc_ps2;
+	struct arc_ps2_data *arc_ps2 = platform_get_drvdata(pdev);
 	int i;
 
-	arc_ps2 = platform_get_drvdata(dev);
-	for (i = 0; i < ARC_PS2_PORTS; i++) {
-		/* turn off any interrupts */
-		outl(0, arc_ps2->port[i].status);
+	for (i = 0; i < ARC_PS2_PORTS; i++)
 		serio_unregister_port(arc_ps2->port[i].io);
-	}
 
 	free_irq(arc_ps2->irq, arc_ps2);
-	iounmap((void __iomem *) arc_ps2->addr);
+	iounmap(arc_ps2->addr);
 	release_mem_region(arc_ps2->iomem_res->start,
 			   resource_size(arc_ps2->iomem_res));
 
-	pr_debug("%s: interrupt count = %i\n", __func__, arc_ps2->total_int);
-	pr_debug("%s: frame error count = %i\n", __func__,
-	       arc_ps2->frame_error);
-	pr_debug("%s: buffer overflow count = %i\n", __func__,
-	       arc_ps2->buf_overflow);
+	dev_dbg(&pdev->dev, "interrupt count = %i\n", arc_ps2->total_int);
+	dev_dbg(&pdev->dev, "frame error count = %i\n", arc_ps2->frame_error);
+	dev_dbg(&pdev->dev, "buffer overflow count = %i\n",
+		arc_ps2->buf_overflow);
 
 	kfree(arc_ps2);
 
@@ -259,27 +264,16 @@ static int __devexit arc_ps2_remove(struct platform_device *dev)
 }
 
 static struct platform_driver arc_ps2_driver = {
-	.driver = {
-		   .name = "arc_ps2",
-		   .owner = THIS_MODULE,
-		   },
-	.probe = arc_ps2_probe,
-	.remove = __devexit_p(arc_ps2_remove),
+	.driver	= {
+		.name	= "arc_ps2",
+		.owner	= THIS_MODULE,
+	},
+	.probe	= arc_ps2_probe,
+	.remove	= __devexit_p(arc_ps2_remove),
 };
 
-static int __init arc_ps2_init(void)
-{
-	return platform_driver_register(&arc_ps2_driver);
-}
+module_platform_driver(arc_ps2_driver);
 
-static void __exit arc_ps2_exit(void)
-{
-	platform_driver_unregister(&arc_ps2_driver);
-}
-
-/* --------------------------------------------------------------------- */
-module_init(arc_ps2_init);
-module_exit(arc_ps2_exit);
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Pavel Sokolov <pavel.sokolov@viragelogic.com>");
+MODULE_AUTHOR("Pavel Sokolov <psokolov@synopsys.com>");
 MODULE_DESCRIPTION("ARC PS/2 Driver");
