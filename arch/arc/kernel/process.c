bb865ffa02b9d4433fd12b66bd52412433ff6597
Vineet Gupta <vgupta@synopsys.com>
2013-01-01 17:14:55 +0530

    ARC: [Review] Use right APIs in cpu_idle()
    Tracks
    * 1268fbc746 "nohz: Remove tick_nohz_idle_enter_norcu()" and
    * bd2f55361f "sched/rt: Use schedule_preempt_disabled()"
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/kernel/process.c
+++ arch/arc/kernel/process.c
@@ -86,15 +86,15 @@ void cpu_idle(void)
 	/* endless idle loop with no priority at all */
 	while (1) {
 		tick_nohz_idle_enter();
+		rcu_idle_enter();
 
 		while (!need_resched())
 			arch_idle();
 
+		rcu_idle_exit();
 		tick_nohz_idle_exit();
 
-		preempt_enable_no_resched();
-		schedule();
-		preempt_disable();
+		schedule_preempt_disabled();
 	}
 }
 
