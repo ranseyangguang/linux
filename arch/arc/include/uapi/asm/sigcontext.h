c0adfbbcf0d5f9f7dcadfe15bf4cc9cbdd8089c0
Vineet Gupta <vgupta@synopsys.com>
2013-02-20 17:16:18 +0530

    ARC: pt_regs "leak" into userspace ABI (partial backported from 3.9)
    We can't do the full backport as it breaks the ABI. So we keep ABI
    compatibility, but remove the reference to pt_regs
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/include/uapi/asm/sigcontext.h
+++ arch/arc/include/uapi/asm/sigcontext.h
@@ -13,11 +13,22 @@
 
 /*
  * Signal context structure - contains all info to do with the state
- * before the signal handler was invoked.  Note: only add new entries
- * to the end of the structure.
+ * before the signal handler was invoked.
  */
 struct sigcontext {
-	struct pt_regs regs;
+	/*
+	 * open coded pt_regs as it is no longer part of userspace ABI
+	 * But this is binary compatible with pt_regs
+	 * upstream 3.9 does this in cleanest way
+	 */
+	struct {
+		long pad;
+		long bta, lp_start, lp_end, lp_count;
+		long status32, ret, blink, fp, gp;
+		long r12, r11, r10, r9, r8, r7, r6, r5, r4, r3, r2, r1, r0;
+		long sp;
+		long orig_r0, orig_r8_word;
+	} regs;
 };
 
 #endif /* _ASM_ARC_SIGCONTEXT_H */
